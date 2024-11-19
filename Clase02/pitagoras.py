80beb59afcaa65c0c4885b4c856a09d782720fe6
Ariel <41440915+VintaBytes@users.noreply.github.com>
2024-11-19 19:14:36 -0300

    Add files via upload

---
+++ Clase02/pitagoras.py
@@ -0,0 +1,6 @@
+
+nombre = "Juan" 
+apellido = "Perez"
+print(nombre,apellido)
+completo = apellido + ", " + nombre 
+print(completo)
---
+++ Clase02/print1.py
@@ -0,0 +1,7 @@
+print()
+
+a = 3
+print(a)
+
+a = a + 2
+print(a)
